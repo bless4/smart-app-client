0de1b8519e8cadf75262a1d488874e9aa40b7390
Martin Sone <36552429+bless4@users.noreply.github.com>
2019-05-01 10:56:00 +0200

    added appi endpoint

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch(' https://git.heroku.com/calm-wave-48764.git/signin', {
+    fetch('https://calm-wave-48764.herokuapp.com/ /signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
